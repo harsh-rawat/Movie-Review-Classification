bd1ca68ca8a30f3ca771b0dcdca95ec1e028963a
Harsh Rawat <45080929+harsh-rawat@users.noreply.github.com>
2020-01-22 21:49:07 -0600

    Update Classifier.java

--- Classifier.java
+++ Classifier.java
@@ -4,8 +4,6 @@ import java.util.Map;
 /**
  * Interface class for the naive bayes classifier. For an explanation of methods, see
  * NaiveBayesClassifierImpl.
- * <p>
- * DO NOT MODIFY
  */
 public interface Classifier {
     void train(List<Instance> trainData, int v);
